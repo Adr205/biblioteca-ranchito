c03f9ff22c412c29ed712c8465acd41f745c4edc
Adr205 <A01242469@itesm.mx>
2021-09-25 23:46:58 -0600

    pasar objeto por ruta y render de book

--- src/app/book-description/book-description.component.ts
+++ src/app/book-description/book-description.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Tag } from '../book-tag/book-tag.component';
 @Component({
   selector: 'app-book-description',
   templateUrl: './book-description.component.html',
   styleUrls: ['./book-description.component.scss'],
 })
 export class BookDescriptionComponent implements OnInit {
-  constructor(public router:Router) {
-
-  }
+  tags: [] = [];
+  titulo : String = "";
+  autor : String = "";
+  sinopsis : String = "";
+  saga : String = "";
+  categoria : String = "";
+  editorial : String = "";
+  fecha : Number = 0;
+  volumen : Number = 0;
+  imagen : String = "";
+  
+  constructor(public router: Router) {}
 
   ngOnInit(): void {
+    // console.log(history.state.data.title)
+    this.titulo = history.state.data.title;
+    this.autor = history.state.data.autor;
+    this.sinopsis = history.state.data.sinopsis;
+    this.saga = history.state.data.saga;
+    this.categoria = history.state.data.categoria;
+    this.editorial = history.state.data.editorial;
+    this.fecha = history.state.data.fecha;
+    this.volumen = history.state.data.volumen;
+    this.imagen = history.state.data.imagen;
   }
 }
