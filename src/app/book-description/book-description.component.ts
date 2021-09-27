e7cf94fbde2be9c3f37c0942b54b008c07b845ed
Adr205 <A01242469@itesm.mx>
2021-09-26 19:55:05 -0600

    responsive

--- src/app/book-description/book-description.component.ts
+++ src/app/book-description/book-description.component.ts
@@ -1,13 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Tag } from '../book-tag/book-tag.component';
+
+export class Tag {
+  public id!: number;
+  public title!: string;
+  public autor!: string;
+  public sinopsis!: string;
+  public saga!: string;
+  public categoria!: string;
+  public editorial!: string;
+  public fecha!: number;
+  public volumen!: number;
+  public imagen!: string;
+
+  constructor(
+    id: number,
+    title: string,
+    autor: string,
+    sinopsis: string,
+    saga: string,
+    categoria: string,
+    editorial: string,
+    fecha: number,
+    volumen: number,
+    imagen: string
+  ) {}
+}
+
 @Component({
   selector: 'app-book-description',
   templateUrl: './book-description.component.html',
   styleUrls: ['./book-description.component.scss'],
 })
 export class BookDescriptionComponent implements OnInit {
-  tags: [] = [];
+  tags: Tag[] = [];
   titulo : String = "";
   autor : String = "";
   sinopsis : String = "";
@@ -18,18 +45,45 @@ export class BookDescriptionComponent implements OnInit {
   volumen : Number = 0;
   imagen : String = "";
   
-  constructor(public router: Router) {}
+  constructor(public router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
     // console.log(history.state.data.title)
-    this.titulo = history.state.data.title;
-    this.autor = history.state.data.autor;
-    this.sinopsis = history.state.data.sinopsis;
-    this.saga = history.state.data.saga;
-    this.categoria = history.state.data.categoria;
-    this.editorial = history.state.data.editorial;
-    this.fecha = history.state.data.fecha;
-    this.volumen = history.state.data.volumen;
-    this.imagen = history.state.data.imagen;
+
+    if (history.state.data.title != null) {
+      this.titulo = history.state.data.title;
+      this.autor = history.state.data.autor;
+      this.sinopsis = history.state.data.sinopsis;
+      this.saga = history.state.data.saga;
+      this.categoria = history.state.data.categoria;
+      this.editorial = history.state.data.editorial;
+      this.fecha = history.state.data.fecha;
+      this.volumen = history.state.data.volumen;
+      this.imagen = history.state.data.imagen;
+    }else{
+      this.getBook();
+    }
+    
   }
+
+public getBook() {
+    return this.http
+      .get<any>('https://biblioteca-ranchito.herokuapp.com/books')
+      .subscribe((response) => {
+        console.log(response);
+        this.tags = response;
+
+        // this.titulo = this.tags.title;
+        this.autor = history.state.data.autor;
+        this.sinopsis = history.state.data.sinopsis;
+        this.saga = history.state.data.saga;
+        this.categoria = history.state.data.categoria;
+        this.editorial = history.state.data.editorial;
+        this.fecha = history.state.data.fecha;
+        this.volumen = history.state.data.volumen;
+        this.imagen = history.state.data.imagen;
+        //console.log(this.tags[0]);
+      });
+    }
+
 }
--- src/app/book-tag/book-tag.component.ts
+++ src/app/book-tag/book-tag.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {  HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
 
 export class Tag {
   public id!: number;
