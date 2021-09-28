696bb4d1edd125f59484c75d26293617e2eec04f
Adr205 <A01242469@itesm.mx>
2021-09-27 18:41:10 -0600

    Responsive description

--- src/app/book-description/book-description.component.ts
+++ src/app/book-description/book-description.component.ts
@@ -1,32 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 import { Tag } from '../book-tag/book-tag.component';
-// export class Tag {
-//   public id!: number;
-//   public title!: string;
-//   public autor!: string;
-//   public sinopsis!: string;
-//   public saga!: string;
-//   public categoria!: string;
-//   public editorial!: string;
-//   public fecha!: number;
-//   public volumen!: number;
-//   public imagen!: string;
-
-//   constructor(
-//     id: number,
-//     title: string,
-//     autor: string,
-//     sinopsis: string,
-//     saga: string,
-//     categoria: string,
-//     editorial: string,
-//     fecha: number,
-//     volumen: number,
-//     imagen: string
-//   ) {}
-// }
 
 @Component({
   selector: 'app-book-description',
@@ -34,28 +10,36 @@ import { Tag } from '../book-tag/book-tag.component';
   styleUrls: ['./book-description.component.scss'],
 })
 export class BookDescriptionComponent implements OnInit {
-  tags!: Tag;
-  id : Number = -1;
-  constructor(public router: Router, private http: HttpClient, private route : ActivatedRoute) {
+  tags: Tag = {} as Tag;
+
+  id: Number = -1;
+  constructor(
+    public router: Router,
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private title: Title,
+  ) {
     this.id = this.route.snapshot.params.id;
-    console.log("ID = " + this.id);
+    // console.log("ID = " + this.id);
   }
 
   ngOnInit(): void {
-    if(history.state.data != null){
+    if (history.state.data != null) {
       this.tags = history.state.data;
-    }else{
+      this.title.setTitle(this.tags.title);
+    } else {
       this.getBook();
     }
+    
   }
 
-public getBook() {
+  public getBook() {
     return this.http
       .get<any>('https://biblioteca-ranchito.herokuapp.com/book/' + this.id)
       .subscribe((response) => {
         console.log(response);
         this.tags = response;
+        this.title.setTitle(this.tags.title);
       });
-    }
-
+  }
 }
